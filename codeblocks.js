7a75637f324e79e94e5fa368e0fd5378751d23ba
Roman Van der Sar <roman0811@live.nl>
2022-01-17 10:50:41 -0800

    Update codeblocks.js

--- codeblocks.js
+++ codeblocks.js
@@ -361,3 +361,86 @@
 //   );
 
 ///////////////////////////////////////////////////////////////////////
+
+// const describeCountry = function (country, population, capitalCity) {
+//   return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
+// };
+
+// const holland = describeCountry("The Netherlands", 17, "Amsterdam");
+// console.log(holland);
+
+/////////////////////////////////////////////////////////////////////
+
+//Random number game code
+
+// let secretNumber = Math.trunc(Math.random() * 20) + 1;
+// let score = 20;
+// let highScore = 0;
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess, typeof guess);
+//   // When nothing is selected
+//   if (!guess) {
+//     document.querySelector('.message').textContent = 'No number!';
+
+//     // When player Wins
+//   } else if (guess === secretNumber) {
+//     document.querySelector('.message').textContent = 'Correct Number!';
+//     document.querySelector('body').style.backgroundColor = '#60b347';
+//     document.querySelector('.number').style.width = '30rem';
+//     document.querySelector('.number').textContent = secretNumber;
+
+//     if (score > highScore) {
+//       highScore = score;
+//       document.querySelector('.highscore').textContent = highScore;
+//     }
+
+//     // When guess is wrong
+//   } else if (guess !== secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent =
+//         guess > secretNumber ? 'Too High!' : 'Too Low!';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You Lost The Game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+
+//   //     // When guess is too high
+//   //   } else if (guess > secretNumber) {
+//   //     if (score > 1) {
+//   //       document.querySelector('.message').textContent = 'Too High!';
+//   //       score--;
+//   //       document.querySelector('.score').textContent = score;
+//   //     } else {
+//   //       document.querySelector('.message').textContent = 'You Lost The Game!';
+//   //       document.querySelector('.score').textContent = 0;
+//   //     }
+//   //     // When guess is too low
+//   //   } else if (guess < secretNumber) {
+//   //     if (score > 1) {
+//   //       document.querySelector('.message').textContent = 'Too Low!';
+//   //       score--;
+//   //       document.querySelector('.score').textContent = score;
+//   //     } else {
+//   //       document.querySelector('.message').textContent = 'You lost the game!';
+//   //       document.querySelector('.score').textContent = 0;
+//   //     }
+//   //   }
+
+//   document.querySelector('.again').addEventListener('click', function () {
+//     score = 20;
+//     let secretNumber = Math.trunc(Math.random() * 20) + 1;
+//     document.querySelector('.message').textContent = 'Start Guessing';
+//     document.querySelector('.score').textContent = score;
+//     document.querySelector('.number').textContent = '?';
+//     document.querySelector('.guess').value = '';
+//     document.querySelector('body').style.backgroundColor = '#222';
+//     document.querySelector('.number').style.width = '15rem';
+//   });
+// });
+
+/////////////////////////////////////////////////////////////////////////
