2e2a4916a27111b596e344160bba3aaa1ce3e876
Roman Van der Sar <roman0811@live.nl>
2022-01-25 10:52:41 -0800

    Update codeblocks.js

--- codeblocks.js
+++ codeblocks.js
@@ -844,3 +844,32 @@
 // checkMiddleSeat('3E');
 
 ///////////////////////////////////////////////////////////////////
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 2324542545,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+//   if (passenger.passport === 2324542545) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+//////////////////////////////////////////////////////////////////////////
