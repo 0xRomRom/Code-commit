eb345271fdc7c3290fa7a7d5239867bd9c70c0d5
Roman Van der Sar <roman0811@live.nl>
2022-01-28 06:18:04 -0800

    Update codeblocks.js

--- codeblocks.js
+++ codeblocks.js
@@ -974,3 +974,27 @@
 //   checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 ////////////////////////////////////////////////////////////////////////
+
+//Filter method
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+
+// console.log(depositsFor);
+
+// const withdrawals = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+
+// console.log(withdrawals);
+
+////////////////////////////////////////////////////////////////////////
