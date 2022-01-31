e91250cd9c894343dd8029a3f5401d698524212d
Roman Van der Sar <roman0811@live.nl>
2022-01-30 19:06:22 -0800

    Update codeblocks.js

--- codeblocks.js
+++ codeblocks.js
@@ -1100,3 +1100,32 @@
 // console.log(Number.isFinite('20X'));
 
 //////////////////////////////////////////////////////////////////////
+
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+
+// console.log(Math.max(5, 18, 25, 11, 2));
+
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// console.log(Math.trunc(Math.random() * 6 + 1));
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// console.log(randomInt(2, 10));
+
+// // Rounding integers
+// console.log(Math.trunc(23, 3));
+
+// console.log(Math.round(23.9));
+
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
+
+// // Rounding decimals
+
+// console.log((2.7).toFixed(0));
+
+//////////////////////////////////////////////////////////////////////
