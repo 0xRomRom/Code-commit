7829cc72fc1b27b7e1abbe74c5de39860699e65a
Roman Van der Sar <roman0811@live.nl>
2022-01-31 06:40:08 -0800

    Update codeblocks.js

--- codeblocks.js
+++ codeblocks.js
@@ -1129,3 +1129,38 @@
 // console.log((2.7).toFixed(0));
 
 //////////////////////////////////////////////////////////////////////
+
+// Create a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('"Mon Jan 31 2022 05:49:18 GMT-0800"'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future);
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// console.log(future.getDate());
+// console.log(future.getHours());
+// console.log(future.getMinutes());
+// console.log(future.getSeconds());
+// console.log(future.getSeconds());
+// console.log(future.toISOString());
+// console.log(future.getTime());
+
+// console.log(new Date(2142285780000));
+
+// console.log(Date.now());
+
+// future.setFullYear(2040);
+// console.log(future);
+
+//////////////////////////////////////////////////////////////////////
