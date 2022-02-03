79c4e64c727bbf1b48836cbf63b71e099a2330c2
Roman Van der Sar <roman0811@live.nl>
2022-02-03 11:41:39 -0800

    Update codeblocks.js

--- codeblocks.js
+++ codeblocks.js
@@ -1230,3 +1230,29 @@
 // // };
 
 //////////////////////////////////////////////////////////////////////
+
+// // rgb(255,255,255)
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1));
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+// console.log(randomColor(0, 255));
+
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("LINK", e.target, e.currentTarget);
+
+//   // Stop propagation
+//   e.stopPropagation();
+// });
+
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("CONTAINER", e.target, e.currentTarget);
+// });
+
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("NAV", e.target, e.currentTarget);
+// });
+
+/////////////////////////////////////////////////////////////////////////
